fbb04297e37645af68e8e55827f686edb0c39e12
Rishabh <rishabhmishra.81E@gmail.com>
2026-03-15 20:18:37 +0530

    updates

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import axios from 'axios'
 
-const API_URL = 'http://localhost:8000'
+const API_URL = import.meta.env.VITE_API_URL || '/api'
 
 const useAuthStore = create((set, get) => ({
   user: null,
